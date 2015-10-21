2003399d548a21111d84324014e09c8d4dfcb6c5
Joseph Leonardo <jealeonardo@gmail.com>
2015-10-21 11:45:28 +0400

    added view detail view updated urls.py added redirect to home view when successfully saved

--- info/urls.py
+++ info/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
+
 
 urlpatterns = patterns('',
-                       # Examples:
                        url(r'^$', 'info.views.home', name='home'),
-
+                       url(r'^view-detail/(?P<detail_id>\d+)/$', 'info.views.view_detail', name='view_detail'),
                        )
--- info/views.py
+++ info/views.py
@@ -1,6 +1,8 @@
 from django.contrib import messages
 from django.shortcuts import render
+from core.shortcuts import reverse_redirect, get_object_or_None
 from info.forms import DetailForm
+from info.models import Detail
 
 
 def home(request):
@@ -9,11 +11,25 @@ def home(request):
     if request.method == 'POST':
         form = DetailForm(request.POST)
         if form.is_valid():
-            form.save()
+            detail = form.save()
             messages.success(request, "Data successfully saved.")
+            return reverse_redirect('info:view_detail', args=[detail.pk])
 
     else:
         form = DetailForm()
 
     args['form'] = form
     return render(request, 'info/home.html', args)
+
+
+def view_detail(request, detail_id):
+    args = {}
+    detail = get_object_or_None(Detail, pk=detail_id, is_active=True)
+
+    if detail:
+        args["detail"] = detail
+        return render(request, 'info/view_detail.html', args)
+    else:
+        messages.warning(request, "unable to locate detail")
+        return reverse_redirect('info:home')
+
